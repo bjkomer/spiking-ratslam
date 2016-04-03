fcb9309ffdf4ea0de52c3c61db2b8fb7d463c074
Brent Komer <brent.komer@gmail.com>
2016-04-03 18:44:34 -0400

    got cyclic integrators mostly working

--- prototyping/2d_cyclic_integrator.py
+++ prototyping/2d_cyclic_integrator.py
@@ -3,45 +3,34 @@ import numpy as np
 import time
 
 DIM_XY=11
-n_neurons = 3000#1000
+n_neurons = 5000#1000
 tau = .1
 
 def integrate(x):
 
-    #val = np.arcsin(x[0])
-
-    #return np.sin(val+x[2]), np.cos(val+x[2]), 0
-
-    return (x[0] + x[1]*x[2]*tau)*1.1, (x[1] - x[0]*x[2]*tau)*1.1
+    return [(x[0] + x[1]*x[4]*tau)*1.1, (x[1] - x[0]*x[4]*tau)*1.1, 
+            (x[2] + x[3]*x[5]*tau)*1.1, (x[3] - x[2]*x[5]*tau)*1.1]
 
 def decoding(x):
 
-    return np.arctan2(x[0], x[1])
-
-    val = x[0]
-    if val > 1:
-        val = 1
-    if val < -1:
-        val = -1
-
-    return np.arcsin(val)
+    return np.arctan2(x[0], x[1]), np.arctan2(x[2], x[3])
 
 model = nengo.Network(seed=13)
 #model.config[nengo.Ensemble].neuron_type = nengo.Direct() #TODO: temp, just use direct for debugging
 with model:
 
 
-    velocity = nengo.Node([0]) # x velocity
-    position = nengo.Ensemble(1000, dimensions=1, neuron_type=nengo.Direct())
-    posecells = nengo.Ensemble(n_neurons, dimensions=3, radius=1)
+    velocity = nengo.Node([0,0]) # x-y velocity
+    position = nengo.Ensemble(1000, dimensions=2, neuron_type=nengo.Direct())
+    posecells = nengo.Ensemble(n_neurons, dimensions=6, radius=1)
 
     #nengo.Connection(velocity, posecells, transform = [[tau,0],[0,tau]])
-    nengo.Connection(velocity, posecells[2])
-    nengo.Connection(posecells, posecells[:2], function=integrate)
+    nengo.Connection(velocity, posecells[-2:])
+    nengo.Connection(posecells, posecells[:-2], function=integrate)
     nengo.Connection(posecells, position, function=decoding)
     
-    initial_stim = nengo.Node(lambda t: 1 if t < 0.15 else 0)
-    nengo.Connection(initial_stim, posecells[0])
+    initial_stim = nengo.Node(lambda t: [1,1] if t < 0.15 else [0,0])
+    nengo.Connection(initial_stim, posecells[[0,2]])
 
 
 if __name__ == '__main__':
